e05fa615bc92377ec68919424b8bb5b0602411a6
Da1nJung <91296636+Da1nJung@users.noreply.github.com>
2021-10-12 15:21:35 +0900

    1012

---
+++ js/common.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+    // 인포그래픽
+
+    $('.chart1').easyPieChart({
+        barColor: '#fff',
+        trackColor: 'rgba(255,255,255,0.2)',
+        scaleColor: 'rgba(0,0,0,0)',
+        lineCap: 'butt',
+        lineWidth: 8,
+        size: 170,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+
+    $('.chart2').easyPieChart({
+        barColor: '#fff',
+        trackColor: 'rgba(255,255,255,0.2)',
+        scaleColor: 'rgba(0,0,0,0)',
+        lineCap: 'butt',
+        lineWidth: 8,
+        size: 170,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+
+    $('.chart3').easyPieChart({
+        barColor: '#fff',
+        trackColor: 'rgba(255,255,255,0.2)',
+        scaleColor: 'rgba(0,0,0,0)',
+        lineCap: 'butt',
+        lineWidth: 8,
+        size: 170,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+
+    $('.chart4').easyPieChart({
+        barColor: '#fff',
+        trackColor: 'rgba(255,255,255,0.2)',
+        scaleColor: 'rgba(0,0,0,0)',
+        lineCap: 'butt',
+        lineWidth: 8,
+        size: 170,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+
+    $('.chart5').easyPieChart({
+        barColor: '#fff',
+        trackColor: 'rgba(255,255,255,0.2)',
+        scaleColor: 'rgba(0,0,0,0)',
+        lineCap: 'butt',
+        lineWidth: 8,
+        size: 170,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+ 
+    
+      
+});
